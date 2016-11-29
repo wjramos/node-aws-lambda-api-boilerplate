c9a3ca5af5b5280e911e72f0dbcb08afb1a0bbfe
Jordan Ramos <jordan.ramos@timeinc.com>
2016-11-28 20:41:17 -0800

    Update handlers

--- src/handlers.js
+++ src/handlers.js
@@ -1,38 +1,12 @@
+import _kebabCase from 'lodash.kebabcase';
+import * as util from './util';
 import * as lib from './lib';
 
 const handlers = {};
 
 // Generate new handler per lib function
 for (let fn in lib) {
-  handlers[fn.toLowerCase()] = createHandler(lib[fn]);
+  handlers[_kebabCase(fn)] = util.createHandler(lib[fn]);
 }
 
 export default handlers;
-
-export function createHandler(Clss) {
-  return async (params, ctx, cb) => {
-    for (let param in params) {
-      console.log(`${param}: ${params[param]}`);
-    }
-
-    try {
-      if (!isClass(Clss)) { throw new Error(`${Clss.name} is not a class`) };
-      const result = await (new Clss(params));
-      return cb(null, result);
-
-    } catch(err) {
-      console.error(err);
-      return cb(err);
-    }
-  };
-}
-
-export function isClass(fn) {
-  if ( typeof fn === 'function' && fn.prototype.constructor === fn ) {
-    try {
-      return !!new fn();
-    } catch (e) {}
-  }
-
-  return false;
-}
